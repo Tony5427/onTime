10c7a1f3b2e25f232aafc67b85efc7cf90be916a
Tony5427 <tonychenjs@gmail.com>
2021-01-08 20:49:46 -0600

    almost v2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,37 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final int LEFT_1 = 0;
+    public static final int LEFT_2 = 1;
+    public static final int RIGHT_1 = 2;
+    public static final int RIGHT_2 = 3;
+
+    public static final int INTAKE_LEFT = 4;
+    public static final int INTAKE_RIGHT = 5;
+    public static final int INTAKE_IN_BUTTON = 7;
+    public static final int INTAKE_OUT_BUTTON = 8;
+    public static final double INTAKE_IN_SPEED = 0.5;
+    public static final double INTAKE_OUT_SPEED = -0.8;
+
+    public static final int HATCH_BUTTON = 9;
+    public static final int HATCH_SOLENOID = 10;
+    public static final boolean HATCH_DIRECTION = true;
+
+    public static final int ARM_POTENT = 11;
+    public static final int ARM_UP = 12;
+    public static final int ARM_DOWN = 13;
+    public static final int ARM_CONTROL = 14;
+    public static final double ARM_TOP = 69;
+    public static final double ARM_BOTTOM = 109.5;
+    public static final double ARM_SPEED = 0.5;
+    
+    public static final double WRIST_TOP = 5.5;
+    public static final double WRIST_BOTTOM = 56.4;
+    public static final int WRIST_POTENT = 15;
+    public static final int WRIST_CONTROL = 16;
+    public static final int WRIST_UP = 17;
+    public static final int WRIST_DOWN = 18;
+    public static final double WRIST_SPEED = 0.5;
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,17 +7,21 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.commands.DriveWithJoystick;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -35,17 +39,37 @@ public class RobotContainer {
   private SpeedControllerGroup left, right;
   private DifferentialDrive drive;
   private static DriveTrain driveTrain;
+
   private Joystick joystick;
+
+  private SpeedController intakeLeft, intakeRight;
+  private Intake intake;
+  private Button intakeIn, intakeOut;
+
+  private HatchIntake hatch;
+  private Solenoid hatchSolenoid;
+  private Button hatchButton;
+
+  private AnalogPotentiometer armPotent;
+  private SpeedController armController;
+  private Arm arm;
+  private Button armUp, armDown;
+
+  private AnalogPotentiometer wristPotent;
+  private SpeedController wristController;
+  private Wrist wrist;
+  private Button wristUp, wristDown;
+
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     // Configure the button bindings
-    left1 = new SteelTalonsController(0, false, 1);
-    left2 = new SteelTalonsController(1, false, 1);
-    right1 = new SteelTalonsController(2, false, 1);
-    right2 = new SteelTalonsController(3, false, 1);
+    left1 = new SteelTalonsController(Constants.LEFT_1, false, 1);
+    left2 = new SteelTalonsController(Constants.LEFT_2, false, 1);
+    right1 = new SteelTalonsController(Constants.RIGHT_1, false, 1);
+    right2 = new SteelTalonsController(Constants.RIGHT_2, false, 1);
 
     left = new SpeedControllerGroup(left1, left2);
     right = new SpeedControllerGroup(right1, right2);
@@ -53,6 +77,21 @@ public class RobotContainer {
     drive = new DifferentialDrive(left, right);
     driveTrain = new DriveTrain(left, right, drive);
     driveTrain.setDefaultCommand(new DriveWithJoystick());
+
+    intakeLeft = new SteelTalonsController(Constants.INTAKE_LEFT, false, 1);
+    intakeRight = new SteelTalonsController(Constants.INTAKE_RIGHT, false, 1);
+    intake = new Intake(intakeLeft, intakeRight);
+
+    armPotent = new AnalogPotentiometer(Constants.ARM_POTENT);
+    armController = new SteelTalonsController(Constants.ARM_CONTROL, false, 1);
+    arm = new Arm(armController, armPotent);
+
+    hatchSolenoid = new Solenoid(Constants.HATCH_SOLENOID);
+    hatch = new HatchIntake(Robot.m_robotContainer.getJoystick().getRawButtonPressed(Constants.INTAKE_IN_BUTTON), hatchSolenoid);
+
+    wristPotent = new AnalogPotentiometer(Constants.WRIST_POTENT);
+    wristController = new SteelTalonsController(Constants.WRIST_CONTROL, false, 1);
+    wrist = new Wrist(wristController, wristPotent);
 
     configureButtonBindings();
   }
@@ -65,7 +104,25 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     joystick = new Joystick(0);
+    intakeIn = new JoystickButton(joystick, Constants.INTAKE_IN_BUTTON);
+    intakeOut = new JoystickButton(joystick, Constants.INTAKE_OUT_BUTTON);
 
+    intakeIn.whileHeld(new MoveIntake(Constants.INTAKE_IN_SPEED));
+    intakeOut.whileHeld(new MoveIntake(Constants.INTAKE_OUT_SPEED));
+
+    hatchButton = new JoystickButton(joystick, Constants.HATCH_BUTTON);
+    hatchButton.whileHeld(new Pneumatics(Constants.HATCH_DIRECTION));
+
+    armUp = new JoystickButton(joystick, Constants.ARM_UP);
+    armDown = new JoystickButton(joystick, Constants.ARM_DOWN);
+
+    armUp.whileHeld(new MoveArm(Constants.ARM_SPEED));
+    armDown.whileHeld(new MoveArm(-Constants.ARM_SPEED));
+
+    wristUp = new JoystickButton(joystick, Constants.WRIST_UP);
+    wristDown = new JoystickButton(joystick, Constants.WRIST_DOWN);
+    wristUp.whileHeld(new MoveArm(Constants.WRIST_SPEED));
+    wristDown.whileHeld(new MoveArm(-Constants.WRIST_SPEED));
   }
 
 
@@ -85,5 +142,21 @@ public class RobotContainer {
 
   public Joystick getJoystick(){
     return joystick;
+  }
+
+  public Intake getIntake(){
+    return intake;
+  }
+
+  public HatchIntake getHatch(){
+    return hatch;
+  }
+
+  public Wrist getWrist(){
+    return wrist;
+  }
+
+  public Arm getArm(){
+    return arm;
   }
 }
